2f404a8b04df929c66bfe3d75038563044016d26
Jason <jaronemo@msn.com>
2021-02-22 22:34:22 +0800

    Add files via upload
    加入貿易條件、付款條件、增加 權限組人員、選單部份修正。

---
+++ lancer_quote/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "lancer Quote",
+    "version": "1.0",
+    "summary": "義成工廠股份有限公司 報價系統. ",
+    "depends": ["base"],
+    "category": "Sale",
+    "description": """義成工廠股份有限公司 報價系統""",
+    "data": [
+        'data/lancer_base_system.xml',
+        'security/ir.model.access.csv',
+        'views/payment_term.xml',
+        'views/lancer_incoterms.xml',
+        'views/menu.xml',
+    ],
+    "active": False,
+    "installable": True,
+}
+
---
+++ lancer_quote/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import payment_term
+from . import lancer_incoterms
